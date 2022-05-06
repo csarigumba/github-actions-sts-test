7d4b1a900dd7b4aa976cd47fd6e24576453ed23d
AzureAD\BELTRANJERIK <jerik.beltran@gecogeco.com>
2022-05-06 10:31:04 +0800

    NMRC: Setup Swagger Documentation

---
+++ src/main/java/jp/co/suntory/nmrc/application/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package jp.co.suntory.nmrc.application.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+    
+	@Value("${spring.application.name}")
+	private String appName;
+	@Value("${spring.application.version}")
+	private String appVersion;
+
+	/**
+	 * Method representing the bean definition for {@link OpenAPI}
+	 */
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title(appName).description("My Data Center API").version(appVersion));
+	}
+	
+}
--- src/main/java/jp/co/suntory/nmrc/application/controller/TestController.java
+++ src/main/java/jp/co/suntory/nmrc/application/controller/TestController.java
@@ -1,6 +1,7 @@
 package jp.co.suntory.nmrc.application.controller;
 
 import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +13,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jp.co.suntory.nmrc.application.resource.TestRequest;
 
 @RestController
 @RequestMapping("/v1/test")
+@Tag(name = "Test API")
 public class TestController {
 
     @GetMapping
+    @Operation(description = "SearchAPI - Test Search API")
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "400", description = "Invalid parameter."),
+    		@ApiResponse(responseCode = "404", description = "The requested resource doesn't exist.")
+    })
     @ResponseStatus(HttpStatus.OK)
     public String search(@RequestParam(required = false, name = "optionalParam") String optionalParam,
             @RequestParam(name = "requiredParam") String requiredParam) {
@@ -26,6 +38,10 @@ public class TestController {
     }
 
     @GetMapping("/{id}")
+    @Operation(description = "ViewAPI - Test View API")
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "400", description = "Invalid parameter."),
+    })
     @ResponseStatus(HttpStatus.OK)
     public String view(@PathVariable final int id) {
         return "Test View: id = " + id;
@@ -35,18 +51,34 @@ public class TestController {
      * For simplicity, I make the return type as void.
      */
     @PostMapping
+    @Operation(description = "SaveAPI - Test Save API")
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "400", description = "Invalid parameter."),
+    		@ApiResponse(responseCode = "404", description = "The requested resource doesn't exist.")
+    })
     @ResponseStatus(HttpStatus.CREATED)
     public void save(@RequestBody @Valid TestRequest request) {
         System.out.println("Test Save request: " + request);
     }
 
     @PutMapping
+    @Operation(description = "UpdateAPI - Test Update API")
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "204", description = "The requested resource was successfully updated."),
+    		@ApiResponse(responseCode = "400", description = "Invalid parameter.")
+    })    
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody @Valid TestRequest request) {
         System.out.println("Test Update request: " + request);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(description = "DeleteAPI - Test Delete API")
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "204", description = "The requested resource was successfully deleted."),
+    		@ApiResponse(responseCode = "400", description = "Invalid parameter."),
+    		@ApiResponse(responseCode = "404", description = "The requested resource doesn't exist.")
+    })    
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable final int id) {
         System.out.println("Test Delete: id = " + id);
