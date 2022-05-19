f432756decc8420db6356d5060e20aa6e71a11fe
Sidoy <cediesarigumba@gmail.com>
2022-05-19 14:46:40 +0800

    Update entrypoint_prod.sh

--- scripts/entrypoint_prod.sh
+++ scripts/entrypoint_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn -Dflyway.configFiles=src/main/resources/application-prod.properties flyway:migrate
+mvn -Dflyway.configFiles=BOOT-INF/classes/application-prod.properties flyway:migrate
 java -jar nomurec-1.0.jar
